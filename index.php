7f755c5fca85e4c22e8755361523aa4c7e9dcc45
Andreas <arizkirizaldi@email.unikom.ac.id>
2019-12-11 19:22:22 +0700

    test logout

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+        alert('sedang login')
     }
 
     function signOut() {
@@ -24,42 +25,5 @@
             console.log('User signed out.');
         });
     }
-
-    /**
-     * The Sign-In client object.
-     */
-    var auth2;
-
-    /**
-     * Initializes the Sign-In client.
-     */
-    var initClient = function() {
-        gapi.load('auth2', function(){
-            /**
-             * Retrieve the singleton for the GoogleAuth library and set up the
-             * client.
-             */
-            auth2 = gapi.auth2.init({
-                client_id: '887949345231-o6jlfqlgl441onipe21vdusem72sp6or.apps.googleusercontent.com'
-            });
-
-            // Attach the click handler to the sign-in button
-            auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
-        });
-    };
-
-    /**
-     * Handle successful sign-ins.
-     */
-    var onSuccess = function(user) {
-        console.log('Signed in as ' + user.getBasicProfile().getName());
-    };
-
-    /**
-     * Handle sign-in failures.
-     */
-    var onFailure = function(error) {
-        console.log(error);
-    };
 </script>
 </html>
