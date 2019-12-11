de6dbcb17fc0bc043c5571f5c943c61311869690
Andreas <arizkirizaldi@email.unikom.ac.id>
2019-12-11 19:19:11 +0700

    test logout

--- index.php
+++ index.php
@@ -10,20 +10,12 @@
 
 </body>
 <script>
-    var auth2= gapi.auth.init();
-    if (auth2.isSignedIn.get()) {
-        alert("Sedang Login!");
-    }
     function onSignIn(googleUser) {
         var profile = googleUser.getBasicProfile();
         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
         console.log('Name: ' + profile.getName());
         console.log('Image URL: ' + profile.getImageUrl());
         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    }
-    function onSuccess(googleUser){
-        var profile = googleUser.getBasicProfile();
-        alert('Selamat Datang, '+profile.getName());
     }
 
     function signOut() {
@@ -32,5 +24,42 @@
             console.log('User signed out.');
         });
     }
+
+    /**
+     * The Sign-In client object.
+     */
+    var auth2;
+
+    /**
+     * Initializes the Sign-In client.
+     */
+    var initClient = function() {
+        gapi.load('auth2', function(){
+            /**
+             * Retrieve the singleton for the GoogleAuth library and set up the
+             * client.
+             */
+            auth2 = gapi.auth2.init({
+                client_id: '887949345231-o6jlfqlgl441onipe21vdusem72sp6or.apps.googleusercontent.com'
+            });
+
+            // Attach the click handler to the sign-in button
+            auth2.attachClickHandler('signin-button', {}, onSuccess, onFailure);
+        });
+    };
+
+    /**
+     * Handle successful sign-ins.
+     */
+    var onSuccess = function(user) {
+        console.log('Signed in as ' + user.getBasicProfile().getName());
+    };
+
+    /**
+     * Handle sign-in failures.
+     */
+    var onFailure = function(error) {
+        console.log(error);
+    };
 </script>
 </html>
